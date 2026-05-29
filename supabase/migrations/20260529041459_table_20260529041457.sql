99b633d3b520d7629922cce7a3cda21446ef6be0
Timothy Lim <tim.lim@supabase.io>
2026-05-29 12:15:03 +0800

    Add table_20260529041457

---
+++ supabase/migrations/20260529041459_table_20260529041457.sql
@@ -0,0 +1,10 @@
+create table if not exists table_20260529041457  (
+        id bigint primary key generated always as identity,
+        name text not null,
+        email text,
+        created_at timestamptz default now()
+        );
+
+  alter table table_20260529041457 enable row level security;
+
+  
